2fd39bf53ae614646fcbdc905a78b45e7b85fc0a
ugozeal <47087777+ugozeal@users.noreply.github.com>
2019-09-20 08:54:03 +0100

    did identation in the index.js

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ $.ajax({
           <td>${elem.status}</td>
           <td><input type="button" class="btn btn-yellow btn-general" onclick="onePatient(${
             elem.id
-          })" value="Edit">&nbsp; <input type="button" class="btn btn-general btn-outline-danger" onclick="deletePatient(${
+          })" value="Edit">&nbsp; <input type="button" class="btn btn-general btn-outline-danger" 
+          onclick="deletePatient(${
         elem.id
       })" value="delete"> </td>
       </tr>`
@@ -75,7 +76,6 @@ $.ajax({
 });
 
 //Patient Search
-//function searchPatient(i) {
 $("#search-form").on("submit", function(e) {
   e.preventDefault(e);
   let pNum = $("#search-number").val();
@@ -85,16 +85,6 @@ $("#search-form").on("submit", function(e) {
     url: "http://localhost:3000/patients/",
     method: "get"
   }).done(resp => {
-    // resp.forEach((elem, i) => {
-    //   if (elem.telephone === pNum && elem.email === pEmail) {
-    //     patientExist = true;
-    //   }
-    //   if (patientExist) {
-    //     console.log(elem.name, elem.email, i + 1);
-    //   } else {
-    //     console.log("No such user");
-    //   }
-    // });
     let name = "";
     let age = "";
     let sickness = "";
@@ -120,7 +110,10 @@ $("#search-form").on("submit", function(e) {
           <td>${age}</td>
           <td>${sickness}</td>
           <td>${status}</td>
-          <td><input type="button" class="btn btn-yellow btn-general" onclick="onePatient(${id})" value="Edit">&nbsp; <input type="button" class="btn btn-danger btn-general" onclick="deletePatient(${id})" value="delete"> </td>
+          <td><input type="button" class="btn btn-yellow btn-general" 
+          onclick="onePatient(${id})" value="Edit">&nbsp; 
+          <input type="button" class="btn btn-danger btn-general" 
+          onclick="deletePatient(${id})" value="delete"> </td>
       </tr>
           `);
       // alert("Exist");
@@ -130,7 +123,6 @@ $("#search-form").on("submit", function(e) {
     }
   });
 });
-// }
 
 // Read Single Patient
 function onePatient(i) {
@@ -146,26 +138,33 @@ function onePatient(i) {
       <h4 class="text-center" style="font-size:24px">Update Patient Info</h4><br>
       <div class="row">
       <div class="col-md-6">
-      <input type="text" id="patient-name1" value="${resp.name}" class="form-control" placeholder="Patient Name"><br>
+      <input type="text" id="patient-name1" value="${resp.name}" 
+      class="form-control" placeholder="Patient Name"><br>
       </div>
       
       <div class="col-md-6">
-      <input type="email" id="patient-email1" value="${resp.email}" class="form-control" placeholder="Patient Email"><br>
+      <input type="email" id="patient-email1" value="${resp.email}" 
+      class="form-control" placeholder="Patient Email"><br>
       </div>
       <div class="col-md-6">
-      <input type="tel" id="patient-mobile1" value="${resp.telephone}" class="form-control" placeholder="PhoneNumber"><br>
+      <input type="tel" id="patient-mobile1" value="${resp.telephone}" 
+      class="form-control" placeholder="PhoneNumber"><br>
       </div>
       <div class="col-md-6">
-      <input type="number" id="patient-age1" value="${resp.age}" class="form-control" placeholder="Patient Age"><br>
+      <input type="number" id="patient-age1" value="${resp.age}" 
+      class="form-control" placeholder="Patient Age"><br>
       </div>
       <div class="col-md-6">
-      <input type="text" id="patient-sickness1" value="${resp.sickness}" class="form-control" placeholder="Patient Sickness"><br>
+      <input type="text" id="patient-sickness1" value="${resp.sickness}" 
+      class="form-control" placeholder="Patient Sickness"><br>
       </div>
       <div class="col-md-6">
-      <input type="text" id="patient-status1" value="${resp.status}" class="form-control" placeholder="Patient Status"><br>
+      <input type="text" id="patient-status1" value="${resp.status}" 
+      class="form-control" placeholder="Patient Status"><br>
       </div>
       </div>
-      <input type="button" value="update" class="btn btn-yellow btn-general" onclick="upDateInfo(${resp.id})">
+      <input type="button" value="update" class="btn btn-yellow btn-general" 
+      onclick="upDateInfo(${resp.id})">
       </div>
       `
     );
